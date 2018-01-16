5a2319a1c5f1e3e696b972d92aeb550dd087fda1
Jim Garlick <jrgarlick@gmail.com>
2018-01-15 23:11:52 -0500

    Attempt at fixing reported issues
    Added support for Background statements (ugly). Looked into repeated
    Scenario description. Couldn't replicate, but will ask for more details.
    Will also ask for more info on the null pointer issue

--- src/main/java/info/cukes/formatter/xls/WorksheetSession.java
+++ src/main/java/info/cukes/formatter/xls/WorksheetSession.java
@@ -1,8 +1,10 @@
 package info.cukes.formatter.xls;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.poi.hssf.usermodel.HSSFClientAnchor;
 import org.apache.poi.ss.usermodel.Cell;
@@ -18,22 +20,23 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 public class WorksheetSession {
 
-    protected WorkbookSession workbookSession;
-    protected Workbook workbook;
-    protected Sheet worksheet;
-    protected CreationHelper creationHelper;
-    protected Drawing drawing;
+    public WorkbookSession workbookSession;
+    public Workbook workbook;
+    public Sheet worksheet;
+    public CreationHelper creationHelper;
+    public Drawing drawing;
+    public Set<String> worksheetNames = new HashSet<String>();
 
 
     public WorksheetSession(WorkbookSession workbookSession, String worksheetName) {
         this.workbookSession = workbookSession;
         this.workbook = workbookSession.getWorkbook();
-        this.worksheet = workbook.createSheet(worksheetName);
+        this.worksheet = workbook.createSheet(getUniqueWorksheetName(worksheetName));
         this.creationHelper = workbook.getCreationHelper();
         this.drawing = worksheet.createDrawingPatriarch();
     }
 
-    protected Row getRow(int rownum) {
+    public Row getRow(int rownum) {
         Row row = worksheet.getRow(rownum);
         if (row == null) {
             row = worksheet.createRow(rownum);
@@ -41,16 +44,16 @@ public class WorksheetSession {
         return row;
     }
 
-    protected Row setRow(int rowNum, Object ... cells) {
+    public Row setRow(int rowNum, Object ... cells) {
         return null;
     }
 
-    protected Cell getCell(int rownum, int colnum) {
+    public Cell getCell(int rownum, int colnum) {
         Row row = getRow(rownum);
         return getCell(row, colnum);
     }
 
-    protected Cell getCell(Row row, int colnum) {
+    public Cell getCell(Row row, int colnum) {
         Cell cell = row.getCell(colnum);
         if (cell == null) {
             cell = row.createCell(colnum);
@@ -58,30 +61,62 @@ public class WorksheetSession {
         return cell;
     }
 
-    protected Cell setCell(int rownum, int colnum, Object value) {
-        Row row = getRow(rownum);
-        Cell cell = getCell(row, colnum);
+//    public Cell setCell(int rownum, int colnum, Object value) {
+//        Cell cell = getCell(rownum, colnum);
+//
+//        if (value == null) {
+//            cell.setCellValue((String) null); //blank out the cell
+//        } else {
+//            cell.setCellValue(String.valueOf(value));
+//        }
+//
+//        return cell;
+//    }
 
-        if (value == null) {
-            cell.setCellValue((String) null); //blank out the cell
-        } else if (value instanceof Integer) {
-            cell.setCellValue((Integer) value);
-        } else if (value instanceof Date) {
-            cell.setCellValue((Date) value);
-        } else if (value instanceof Double) {
-            cell.setCellValue((Double) value);
-        } else if (value instanceof RichTextString) {
-            cell.setCellValue((RichTextString) value);
-        } else if (value instanceof Hyperlink) {
-            cell.setHyperlink((Hyperlink) value);
-        } else {
-            cell.setCellValue(String.valueOf(value));
-        }
-
+    public Cell setCellEmpty(int rownum, int colnum) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setCellValue((String) null);
         return cell;
     }
 
-    protected Comment setCellComment(Cell cell, String commentText, String commentAuthor) {
+
+    public Cell setCell(int rownum, int colnum, String value) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setCellValue(value);
+        return cell;
+    }
+
+    public Cell setCell(int rownum, int colnum, Date value) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setCellValue(value);
+        return cell;
+    }
+
+    public Cell setCell(int rownum, int colnum, Integer value) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setCellValue(value);
+        return cell;
+    }
+
+    public Cell setCell(int rownum, int colnum, Double value) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setCellValue(value);
+        return cell;
+    }
+
+    public Cell setCell(int rownum, int colnum, RichTextString value) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setCellValue(value);
+        return cell;
+    }
+
+    public Cell setCell(int rownum, int colnum, Hyperlink value) {
+        Cell cell = getCell(rownum, colnum);
+        cell.setHyperlink(value);
+        return cell;
+    }
+
+    public Comment setCellComment(Cell cell, String commentText, String commentAuthor) {
         HSSFClientAnchor anchor = new HSSFClientAnchor(100, 100, 100, 100, (short)1, 1, (short) 6, 5);
         Comment comment = drawing.createCellComment(anchor);
         comment.setString(creationHelper.createRichTextString(commentText));
@@ -90,7 +125,7 @@ public class WorksheetSession {
         return comment;
     }
 
-    protected void formatCell(Cell cell, String formatIdentifier) {
+    public void formatCell(Cell cell, String formatIdentifier) {
         Map<String, CellStyle> styles = workbookSession.getCellStyles();
         CellStyle style = styles.get(formatIdentifier);
         if (style != null) {
@@ -98,12 +133,19 @@ public class WorksheetSession {
         }
     }
 
-    protected void formatCells(List<Cell> cells, String formatIdentifier) {
+    public void formatCells(List<Cell> cells, String formatIdentifier) {
         for (Cell cell : cells) {
             formatCell(cell, formatIdentifier);
         }
     }
 
-
+    private String getUniqueWorksheetName(String worksheetName) {
+        int i = 1;
+        String currentWorksheetName = worksheetName;
+        while (workbook.getSheet(currentWorksheetName) != null) {
+            currentWorksheetName = worksheetName + "-" + i++;
+        }
+        return currentWorksheetName;
+    }
 
 }
